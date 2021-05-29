d432a5262c457dcbf1676e2c26b60c642e834760
PriontoAbdullah <priontosoft@gmail.com>
2021-05-29 11:33:01 +0600

    All models are created

---
+++ models/Profile.js
@@ -0,0 +1,52 @@
+// User, title, bio, profilePic, links, posts, bookmarks,
+
+const { Schema, model } = require('mongoose');
+const User = require('./User');
+const Post = require('./Post');
+
+const profileSchema = new Schema(
+	{
+		user: {
+			type: Schema.Types.ObjectId,
+			ref: User,
+			userSchema,
+			required: true
+		},
+		title: {
+			type: String,
+			trim: true,
+			maxlength: 100
+		},
+		bio: {
+			type: String,
+			trim: true,
+			maxlength: 500
+		},
+		profilePic: String,
+		links: {
+			website: String,
+			facebook: String,
+			twitter: String,
+			github: String
+		},
+		posts: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: Post
+			}
+		],
+		bookmarks: [
+			{
+				type: Schema.Types.ObjectId,
+				ref: Post
+			}
+		]
+	},
+	{
+		timestamps: true
+	}
+);
+
+const Profile = model('Profile', profileSchema);
+
+module.exports = Profile;
---
+++ models/User.js
@@ -0,0 +1,35 @@
+// Name, Email, Password and Profile
+
+const { Schema, model, isValidObjectId } = require('mongoose');
+const Profile = require('./Profile');
+
+const userSchema = new Schema(
+	{
+		name: {
+			type: String,
+			trim: true,
+			maxlength: 30,
+			required: true
+		},
+		email: {
+			type: String,
+			trim: true,
+			required: true
+		},
+		password: {
+			type: String,
+			required: true
+		},
+		profile: {
+			type: Schema.Types.ObjectId,
+			ref: Profile
+		}
+	},
+	{
+		timestamps: true
+	}
+);
+
+const User = model('User', userSchema);
+
+module.exports = User;
